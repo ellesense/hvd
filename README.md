49552f4eac083bc204e53c509b6259400764c330
Steve Kim <dkim.steve@gmail.com>
2020-11-08 16:25:22 -0800

    add restrictive nicknames

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Change NOT FOUND page to something else
 
 Add LOCATIONS column
 Ability to filter by locations
+
+Confirmation Instruction Email - Koreanify
+
+Can change email address later
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   validates :nickname, presence: true, uniqueness: true
+  validate :reserved_words
+
+  def reserved_words
+      if (/admin/i.match(self.nickname) || /manager/i.match(self.nickname) || /주인/.match(self.nickname) || /관리자/.match(self.nickname) || /매니저/.match(self.nickname) || /오너/.match(self.nickname) || /administrator/i.match(self.nickname) || /exec/i.match(self.nickname) || /ceo/i.match(self.nickname) || /operator/i.match(self.nickname))
+        errors.add(:nickname, "사용 금지된 닉네임입니다.")
+      end
+  end
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
