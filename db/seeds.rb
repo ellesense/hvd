2dec09428a129413a3bbc308f56a2a12ea269bfc
Steve Kim <dkim.steve@gmail.com>
2020-11-11 11:31:44 -0800

    sort categories by created_at DESC

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,47 +2,30 @@
 # Updating all existing posts
 # Post.update_all(:locations => ["Surrey", "Langley"])
 
-# 식사 & 음주
-Category.create(name: 'Eat & Drink')
+# 모든 글
+Category.create(name: 'All')
 
 # 커피 & 티
 Category.create(name: 'Coffee & Tea')
 
-# 기타 액티비티
-Category.create(name: 'Other Activities')
-
-# 스포츠
-Category.create(name: 'Sports')
-
-# 모든 글
-Category.create(name: 'All')
-
-# 익명 온라인 대화
-Category.create(name: 'Anonymous Talks')
+# 식사 & 음주
+Category.create(name: 'Eat & Drink')
 
 # 공부 모임
 Category.create(name: 'Teach & Learn')
 
-# 온라인 게임
-Category.create(name: 'Online Games')
+# 스포츠
+Category.create(name: 'Sports')
+
+# 기타 액티비티
+Category.create(name: 'Other Activities')
 
 # 이벤트 
 Category.create(name: 'Gigs & Events')
 
-# 10.times do |i|
-#     User.create(
-#         email: Faker::Internet.email, 
-#         password: '1234567',
-#         password_confirmation: 'sdofsof
-#         nickname: Faker::Name.name
-#     )
-# end
+# 온라인 게임
+Category.create(name: 'Online Games')
 
-# 100.times do |i|
-#     Post.create(
-#         title: Faker::Games::WorldOfWarcraft.quote, 
-#         description: Faker::Lorem.paragraph(sentence_count: 4), 
-#         user_id: rand(1..10),
-#         category_id: rand(1..8)
-#     )
-# end
+# 익명 온라인 대화
+Category.create(name: 'Anonymous Talks')
+
